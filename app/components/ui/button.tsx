b79e6b9493b8a5b8098b8dc6e5420a5080f9f8db
Virtuoso633 <virtuosoofcoding633@gmail.com>
2025-01-18 07:10:07 +0530

    new Updates

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { cn } from "/Users/sanket/Documents/Chat-heatmap/chat-heatmap/lib/utils"
+import { cn } from "@/lib/utils"
 
 const Button = React.forwardRef<HTMLButtonElement,
   React.ButtonHTMLAttributes<HTMLButtonElement>
